a8886f2f5285111aa2efc35a74365d0f2ff44cbe
sujit patel <sujitworkclg@gmail.com>
2024-08-09 12:02:48 +0530

    Add file data read

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+//main route
 app.get('/', function(req, res) {
     const directoryPath = path.join(__dirname, 'files');
     
@@ -27,9 +28,17 @@ app.get('/', function(req, res) {
     });
 });
 
+//create data file
 app.post('/create', function(req,res) {
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
         res.redirect("/");
+    });
+});
+
+//show file data
+app.get('/files/:fileName', function(req,res) {
+    fs.readFile(`./files/${req.params.fileName}`, "utf-8", function(err, filedata){
+        res.render("showfile", {filetitel: req.params.fileName, filedata});
     });
 });
 
